30ae45214e037972e4f85544c1fd08bb58fb40ed
flof <2712frly@gmail.com>
2026-09-18 00:18:23 +0200

    moved around some values

--- Simulink/main.m
+++ Simulink/main.m
@@ -8,13 +8,16 @@ m1 = 2;
 m2 = 2;
 g = 9.81;
 
+%%% Intrinsic System values
+n = 2;
+x0 = [1 1 1 1]';
 
 %%% calculated values
 a = J1 + J2 + L1^4/4*(m1 + 4*m2) + L2^2*m2/4;
 b = L1*L2*m2;
 c = J2 + L2^2/4*m2;
 
-%%%
+%%% functions
 M = @(q) [a+b*cos(q(2)), c+b/2*cos(q(2));
              c+b/2*cos(q(2)), c];
 
@@ -23,8 +26,3 @@ C = @(q,dq) [0, -b*(q(1)+q(2)/2)*sin(q(2));
 
 G = @(q) [g*m2*(L2/2*cos(q(1)+q(2) + L1*cos(q(1)))) + L1*g*m1*cos(q(2));
           L2/2*m2*g*cos(q(1)+q(2))];
-
-
-%%% System values
-n = 2;
-x0 = [1 1 1 1]';
